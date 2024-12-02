7a22b848238f334aba390c06ebfe267507813fbf
EvaCherkun <evacherkun1@gmail.com>
2024-12-02 17:32:26 +0200

    changes

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ from app import arithmetic_sum
 
 class TestArithmeticSum(unittest.TestCase):
     def test_arithmetic_sum_1(self):
-        self.assertEqual(arithmetic_sum(5), 50)  # Перевірка для 5 членів прогресії
+        self.assertEqual(arithmetic_sum(5), 25)  # Перевірка для 5 членів прогресії
 
     def test_arithmetic_sum_2(self):
         self.assertEqual(arithmetic_sum(0), 0)   # Нуль елементів
 
     def test_arithmetic_sum_3(self):
-        self.assertEqual(arithmetic_sum(1), 4)   # Один елемент, сума = 4
+        self.assertEqual(arithmetic_sum(1), 10)   # Один елемент, сума = 4
 
     def test_arithmetic_sum_4(self):
         self.assertEqual(arithmetic_sum(3), 21)  # 4 + 7 + 10 = 21
